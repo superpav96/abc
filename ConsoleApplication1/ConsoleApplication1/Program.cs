597ffac8acb5d7f8634b8680b2d988c4b96909b9
superpav96 <superpav96@yandex.ru>
2016-12-23 22:40:49 +0300

    gd

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HiWorld");
+
+            //muy
+            Console.WriteLine("Nagmi lubuyu knopku shtoby viiti");
+            Console.ReadKey();
+        }
+    }
+}
